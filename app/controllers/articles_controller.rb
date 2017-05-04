9b6afe389ffcdbb1982b4fe2a00a6eb6d62ad8c6
Sanket Singh <rishav10195@outlook.com>
2017-05-04 20:09:45 +0000

    one of the random commits

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController<ApplicationController
+    def new
+        @article = Article.new
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+        
+    end
+    
+    def update
+    end
+    
+    def create
+        
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice]="Succesful bro!"
+            redirect_to article_path(@article)
+        else
+            render :new
+        end
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+    
+end
