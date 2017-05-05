a6983e653cda8e63bcf952b1101f438d15a4f9ba
Sanket Singh <rishav10195@outlook.com>
2017-05-05 17:09:31 +0000

    Added partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController<ApplicationController
+    
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
     def index
         @articles = Article.all
     end
@@ -45,6 +48,10 @@ class ArticlesController<ApplicationController
     end
     
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
     def article_params
         params.require(:article).permit(:title, :description)
     end
