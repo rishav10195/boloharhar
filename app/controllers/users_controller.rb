ae9f524bec2d2c1c30a05478bcd0659710db8225
Sanket Singh <rishav10195@outlook.com>
2017-05-08 23:13:13 +0000

    User deletion for admin added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     
     before_action :set_user, only: [:edit, :update, :show]
     before_action :require_same_user, only: [:edit, :update]
+    before_action :require_admin, only: [:destroy]
     def new
        @user = User.new 
     end
@@ -38,6 +39,13 @@ class UsersController < ApplicationController
         @user_articles = @user.articles.paginate(page: params[:page], per_page:5)
     end
     
+    def destroy
+        @user= User.find(params[:id]) 
+        @user.destroy
+        flash[:danger] = "User and all articles have been deleted!"
+        redirect_to users_path
+    end
+    
     private
     
     def user_params
@@ -49,8 +57,15 @@ class UsersController < ApplicationController
     end
     
     def require_same_user
-        if current_user != @user
+        if current_user != @user && !current_user.admin?
             flash[:danger] = "You can edit your own account only."
+            redirect_to root_path
+        end
+    end
+    
+    def require_admin
+        if logged_in? && !current_user.admin?
+            flash[:danger] = "You are not AUTHORISED!"
             redirect_to root_path
         end
     end
