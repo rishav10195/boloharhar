4e140fa78fb805bd2a799bb05f69e86d9fcbc85a
Sanket Singh <rishav10195@outlook.com>
2017-05-08 22:54:12 +0000

    admin added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507002504) do
+ActiveRecord::Schema.define(version: 20170508220747) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170507002504) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
