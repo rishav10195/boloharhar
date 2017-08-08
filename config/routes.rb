531bc062e6f168dda7ecfe65eb21df650b768001
Sanket Singh <rishav10195@outlook.com>
2017-08-08 01:29:19 +0000

    Added category functionality using TDD

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     get 'login' => 'sessions#new'
     post 'login' => 'sessions#create'
     delete 'logout' => 'sessions#destroy'
+    
+    resources :categories, except: [:destroy]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
